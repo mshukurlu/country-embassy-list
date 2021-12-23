e5123a6ebbc04537883305b03315d3cc571a58b2
Murad Shukurlu <shukurlu.murad@gmail.com>
2021-12-23 19:12:41 +0400

    Schedule configured for daily 01:00 updating of countries

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('countries:update')->dailyAt('01:00');
     }
 
     /**
