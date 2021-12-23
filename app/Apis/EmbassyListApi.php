085f64ab13777acb79ad0bf26409b298af7c708c
Murad Shukurlu <shukurlu.murad@gmail.com>
2021-12-23 20:28:45 +0400

    slug Variable name changed to meaningful name on EmbassyListApi.php

--- app/Apis/EmbassyListApi.php
+++ app/Apis/EmbassyListApi.php
@@ -6,10 +6,10 @@ namespace App\Apis;
 
 class EmbassyListApi extends ApiAbstract
 {
-    public function __construct($slug)
+    public function __construct($country_slug)
     {
         parent::__construct();
-        $this->setEndPoint('diplomatic-missions/host-country/'.$slug);
+        $this->setEndPoint('diplomatic-missions/host-country/'.$country_slug);
         $this->service_url = $this->getEndPoint();
     }
 
