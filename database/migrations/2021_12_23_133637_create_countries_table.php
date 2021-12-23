35ae9b793338e6f3df73c878d24fe96060bfd714
Murad Shukurlu <shukurlu.murad@gmail.com>
2021-12-23 17:44:20 +0400

    Migration files generated for Countries

---
+++ database/migrations/2021_12_23_133637_create_countries_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("alpha_2_code");
+            $table->string("alpha_3_code");
+            $table->integer('numeric_code');
+            $table->string('slug')->index();
+            $table->string('name');
+            $table->string('display_name');
+            $table->string('keywords');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
